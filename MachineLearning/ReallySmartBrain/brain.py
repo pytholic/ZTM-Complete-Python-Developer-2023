67606bfbf23e3b88162ef8ea17a8d04aba094c82
pytholic <rajahaseeb147@gmail.com>
2023-01-28 15:00:21 +0900

    image classification with imageAI

---
+++ MachineLearning/ReallySmartBrain/brain.py
@@ -0,0 +1,16 @@
+import os
+
+from imageai.Classification import ImageClassification
+
+execution_path = os.path.abspath(os.path.dirname(__file__))
+
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2-b0353104.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(
+    os.path.join(execution_path, "house.jpg"), result_count=5
+)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, " : ", eachProbability)
